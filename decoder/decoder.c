973863f258bc7684389fd3b824b2f7512fe1be5d
angela <manzanaembicicleta@gmail.com>
2023-01-15 12:43:04 -0300

    print compression and decompression proccess info

--- decoder/decoder.c
+++ decoder/decoder.c
@@ -1,7 +1,8 @@
 
 #include "../includes/decoder.h"
 
-void	get_decompress_data(t_decode decode)
+
+void	get_decompress_data(t_decode *decode)
 {
 	size_t	len;
 	int		i;
@@ -10,19 +11,21 @@ void	get_decompress_data(t_decode decode)
 	i = 0;
 	printf("\n");
 	total_len = 0;
-	while (total_len < decode.final_size)
+	decode->info.decoded_bytes = 0;
+	while (total_len < decode->final_size)
 	{
-		while (!*decode.table[i])
+		while (!*decode->table[i])
 			i++;
-		len = strlen(decode.table[i]);
-		while (strncmp((char *)&decode.bin_txt[total_len], decode.table[i], len))
+		len = strlen(decode->table[i]);
+		while (strncmp((char *)&decode->bin_txt[total_len], decode->table[i], len))
 		{
 			i++;
-			while (!*decode.table[i])
+			while (!*decode->table[i])
 				i++;
-			len = strlen(decode.table[i]);
+			len = strlen(decode->table[i]);
 		}
 		printf("%c", (char)i);
+		decode->info.decoded_bytes++;
 		i = 0;
 		total_len += len;
 	}
@@ -46,28 +49,39 @@ UC	*bytes_to_str(UC *txt)
 	return (bin_txt);
 }
 
-void	decoder(t_decode decode)
+void	decoder(t_decode *decode)
 {
 	UC	*txt;
 	int	fd;
 
+	decode->info.encoded_bytes = 0;
 	txt = calloc(sizeof(char), 1024);
 	fd = open("data/compress_file", O_RDONLY);
 	read(fd, txt, 1024);
+	decode->info.encoded_bytes = strlen((char*)txt);
 	close(fd);
-	decode.bin_txt = bytes_to_str(txt);
+	decode->bin_txt = bytes_to_str(txt);
 	get_decompress_data(decode);
-	free(decode.bin_txt);
+	free(decode->bin_txt);
 	free(txt);
 }
 
 void	conect_process(t_data *data)
 {
 	t_decode	decode;
+	clock_t		start;
+	clock_t		end;
+	double		total;
 
+	start = clock();
 	decode.table = data->table;
 	decode.final_size = data->bit_size;
-	decoder(decode);
+	decoder(&decode);
+	end = clock();
+	total = (double)(end - start) / CLOCKS_PER_SEC;
+	printf("\n\t\033[1;36mCompressed bytes:\033[0m %d\n", decode.info.encoded_bytes);
+	printf("\n\t\033[1;36mOriginal text bytes:\033[0m%d\n", decode.info.decoded_bytes);
+	printf("\n\t\033[1;36mTime process takes:\033[0m %f sec\n", total);
 	free_table(data->table);
 	free(data->txt);
 }
--- includes/decoder.h
+++ includes/decoder.h
@@ -1,10 +1,18 @@
 
+#include <time.h>
 #include "encoder.h"
+
+typedef struct p_info
+{
+	int	encoded_bytes;
+	int	decoded_bytes;
+	int	time;
+}	t_info;
 
 typedef struct p_decode
 {
-	UC	*bin_txt;
-	char **table;
-	int final_size;
+	UC		*bin_txt;
+	char	**table;
+	int		final_size;
+	t_info	info;
 }	t_decode;
-
